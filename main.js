7e4a86cd0e8ff358424e1047b1c3c113371d9001
Alisher778 <internet-docs@mail.ru>
2016-10-27 18:43:08 -0400

    made it beauitful contact me page

---
+++ main.js
@@ -0,0 +1,7 @@
+// $(document).ready(function(){
+//   $('.body-wrapper').css('display','none');
+//   $('.loader').fadeOut(7000, function(){
+//     $('.body-wrapper').fadeIn(0);
+//   });
+  
+// });
